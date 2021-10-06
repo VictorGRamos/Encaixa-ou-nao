05df8504c23e0bbbc9f723aca68d71341dd01801
VICTOR_GONCALVES_GUIMARAES_RAMOS <11901942@aluno.cotemig.com.br>
2021-10-06 07:26:43 -0300

    Commit

---
+++ Pit1240/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pit1240
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtd;
+            string a, b;
+            qtd = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= qtd; i++)
+            {
+                string[] v;
+                v = Console.ReadLine().Split(' ');
+                a = v[0];
+                b = v[1];
+                if (a.EndsWith(b))
+                {
+                    Console.WriteLine("encaixa");
+                }
+                else {
+                    Console.WriteLine("nao encaixa");
+                }
+            }
+        }
+    }
+}
